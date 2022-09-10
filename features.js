9ca29894a4ac7bd815c12a929d7918470568839d
neoxr <neoxr@null.net>
2022-09-10 16:41:11 +0700

    release

--- features.js
+++ features.js
@@ -116,6 +116,20 @@ module.exports = async (m, {
             break
          }
 
+         case 'ig': {
+            if (!args || !args[0]) return client.reply(m.chat, Func.example(isPrefix, command, 'https://www.instagram.com/p/CK0tLXyAzEI'), m)
+            if (!args[0].match(/(https:\/\/www.instagram.com)/gi)) return client.reply(m.chat, global.status.invalid, m)
+            client.sendReact(m.chat, '🕒', m.key)
+            let old = new Date()
+            let json = await Api.ig(Func.igFixed(args[0]))
+            if (!json.status) return client.reply(m.chat, Func.jsonFormat(json), m)
+            json.data.map(async v => {
+               client.sendFile(m.chat, v.url, '', `🍟 *Fetching* : ${((new Date - old) * 1)} ms`, m)
+               await Func.delay(1500)
+            })
+            break
+         }
+
          case 'run': {
             let _uptime = process.uptime() * 1000
             let uptime = Func.toTime(_uptime)
@@ -132,7 +146,7 @@ module.exports = async (m, {
             break
          }
 
-         case 'server': { 
+         case 'server': {
             const json = await Func.fetchJson('http://ip-api.com/json')
             return client.reply(m.chat, Func.jsonFormat(json), m)
             break
